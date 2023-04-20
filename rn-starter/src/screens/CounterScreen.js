07b6b88de237d3f2a65c669ab096374ed4e2ab4a
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-20 17:55:11 -0400

    42. State in Action

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CounterScreen = () => {
+  var [counter, setCounter] = useState(0);
   return (
     <View>
-      <Text style={styles.text}>Counter Count:</Text>
+      <Text style={styles.text}>Counter Count:{counter}</Text>
+      <Button
+        title="Increate"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decreate"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
     </View>
   );
 };
