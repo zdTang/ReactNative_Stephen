ab103c3258e71ac35cd8715a78abbeb397230ad4
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-21 10:14:27 -0400

    45. Generating Random Colors

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -5,8 +5,18 @@ const ColorScreen = () => {
   return (
     <View style={styles.textStyle}>
       <Button title="Add a color" />
+      <View
+        style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
+      ></View>
     </View>
   );
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red},${green},${blue})`;
 };
 
 const styles = StyleSheet.create({
