60b898f94cba20fecf5e10d872fe5b349b637f41
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-04-23 16:48:48 -0400

    63. Showing a Text Input

--- rn-starter/src/screens/TextScreen.js
+++ rn-starter/src/screens/TextScreen.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import React from "react";
 
 const TextScreen = () => {
   return (
     <View>
       <Text>TextScreen</Text>
+      <TextInput style={styles.input}></TextInput>
     </View>
   );
 };
 
 export default TextScreen;
 
-const styles = StyleSheet.create({});
+// By default, the TextInput component is a single line and has no border and no background.
+// We need to add a style to make it look like a text field.
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
